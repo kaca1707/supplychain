e5d1057bbb6685174d614bd42652fd71da0c0165
Martin Zumarraga <martinzumur@gmail.com>
2019-03-23 11:45:48 +0100

    update comments

--- src/main/java/upm/softwaredesign/finalproject/model/OrderProxy.java
+++ src/main/java/upm/softwaredesign/finalproject/model/OrderProxy.java
@@ -6,22 +6,26 @@ import java.util.UUID;
 public class OrderProxy {
 
     /*
+    @param blockChinId
     @return set of orders that are in a blockchain
      */
-    public Set<Order> consultChain(){
+    public Set<Order> consultChain(UUID blockChinId){
         //TODO
         return null;
     }
 
-    /*
+    /* Saves an order in a blockchain
+    @param blockChainId
     @param order: Request/Delivery that has to be added to a blockchain
      */
     public void saveOrder(Order order, UUID blockChainId){
         //TODO
     }
 
-    /*
-    @return the status of an order
+    /* checks the status of a blockchain:
+        - Factory Request/Delivery
+        - Producer Request/Delivery
+    @return the status of a blockchain
      */
     public String status(){
         //TODO
