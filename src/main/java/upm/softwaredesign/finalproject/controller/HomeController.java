c8e0b24433eae20494b9ca017a5a62274b735dc0
pipe90010 <felipe.ariasfarfan@gmail.com>
2019-03-28 12:46:28 +0100

    Creation of new branch for persistance and testing of actors creation

--- src/main/java/upm/softwaredesign/finalproject/controller/HomeController.java
+++ src/main/java/upm/softwaredesign/finalproject/controller/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import upm.softwaredesign.finalproject.model.Actor;
+import upm.softwaredesign.finalproject.model.Factory;
+import upm.softwaredesign.finalproject.model.Producer;
+import upm.softwaredesign.finalproject.model.Retailer;
 import upm.softwaredesign.finalproject.repository.ActorRepository;
 
 import java.util.List;
@@ -41,4 +44,39 @@ public class HomeController {
 
         return "redirect:/";
     }
+    
+    @PostMapping("/retailer")
+    public String createNewRetailer() {
+        Actor a = new Retailer();
+        Random r = new Random();
+        final String name = "TEST-Retailer" + r.nextInt();
+        a.setName(name);
+        actorRepository.save(a);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/factory")
+    public String createNewFactory() {
+        Actor a = new Factory();
+        Random r = new Random();
+        final String name = "TEST-Factory" + r.nextInt();
+        a.setName(name);
+        actorRepository.save(a);
+
+        return "redirect:/";
+    }
+    
+    @PostMapping("/producer")
+    public String createNewProducer() {
+        Actor a = new Producer();
+        Random r = new Random();
+        final String name = "TEST-Producer" + r.nextInt();
+        a.setName(name);
+        actorRepository.save(a);
+
+        return "redirect:/";
+    }
+    
+    
 }
