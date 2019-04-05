dff957405a554f9b793d70ab987f749e1d1fb2e1
pipe90010 <37155680+pipe90010@users.noreply.github.com>
2019-04-05 21:03:49 +0200

    save(), update(), delete() added to ActorService + actorServicesTest()

--- src/main/java/upm/softwaredesign/finalproject/service/ActorService.java
+++ src/main/java/upm/softwaredesign/finalproject/service/ActorService.java
@@ -2,83 +2,104 @@ package upm.softwaredesign.finalproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import upm.softwaredesign.finalproject.blockchain.BlockChain;
 import upm.softwaredesign.finalproject.entity.ActorEntity;
+import upm.softwaredesign.finalproject.enums.TransactionStatus;
 import upm.softwaredesign.finalproject.model.Actor;
+import upm.softwaredesign.finalproject.model.Product;
 import upm.softwaredesign.finalproject.model.Retailer;
 import upm.softwaredesign.finalproject.repository.ActorRepository;
 
 import java.util.List;
 
 @Service
-public class ActorService{
+public class ActorService {
 
 	private final ActorRepository repository;
-	
+
 	@Autowired
 	public ActorService(ActorRepository repository) {
 		this.repository = repository;
 	}
 
-	public Actor cast(ActorEntity actorEntity)
-	{
-		// TODO: We are loosing information about actor type here; Actor should be abstract.
-		//  TODO: We need to have a logic to distinguish type of actor and create aprioprate type
+	public Actor cast(ActorEntity actorEntity) {
+		// TODO: We are loosing information about actor type here; Actor should be
+		// abstract.
+		// TODO: We need to have a logic to distinguish type of actor and create
+		// Appropriate type
 		Actor actor = new Retailer();
 		actor.setName(actorEntity.getName());
 		actor.setType(actorEntity.getType());
 		return actor;
 	}
-	public Actor findByType(String type ) {
+
+	public Actor findByType(String type) {
 		List<ActorEntity> actors = this.repository.findAll();
 		for (int i = 0; i < actors.size(); i++) {
-			if(actors.get(i).getType().equals(type))
-			{
+			if (actors.get(i).getType().equals(type)) {
 				return this.cast(actors.get(i));
-				
+
 			}
 		}
 		return null;
 	}
 
-    public ActorEntity save(Actor actor) {
-    	ActorEntity actorEntity = new ActorEntity();
-    	actorEntity.setName(actor.getName());
-    	actorEntity.setType(actor.getType());
-    	return this.repository.save(actorEntity);
-    }
-    /*
-    public void delete(Actor actor) {
-    	
-    	List<ActorEntity> actors = this.repository.findAll();
-		for (int i = 0; i < actors.size(); i++) {
-			if(actors.get(i).getType().equals(actor.getType()))
-			{
-				this.repository.deleteById(actors.get(i).getId());				
+	public ActorEntity save(Actor actor) {
+		ActorEntity actorEntity = new ActorEntity();
+		actorEntity.setName(actor.getName());
+		actorEntity.setType(actor.getType());
+		return this.repository.save(actorEntity);
+	}
+
+	public void delete(Actor actor) {
+
+		List<ActorEntity> actors = this.repository.findAll();
+		for (ActorEntity actorEntity : actors) {
+			if (actorEntity.getId() == actor.getId()) {
+				this.repository.deleteById(actorEntity.getId());
+			}
+		}
+	}
+
+	public void update(Actor ac) {
+		List<ActorEntity> actors = this.repository.findAll();
+		for (ActorEntity actorEntity : actors) {
+			if (actorEntity.getId() == ac.getId()) {
+				actorEntity.setName(ac.getName());
+				actorEntity.setType(ac.getType());
+				this.repository.save(actorEntity);
+			}
+		}
+	}
+
+	public List<Actor> retrieveActors() {
+		List<Actor> actors = null;
+		List<ActorEntity> actorsEntityList = this.repository.findAll();
+		if (actorsEntityList.isEmpty()) {
+			/* return new List<Actor>(); */
+		}
+
+		/*
+		 * for(ActorEntity actorEntity : actorsEntityList) {
+		  }
+		 */
+		return actors;
+	}
+
+	public List<Actor> retrieveActorBytype(String actorType) {
+		List<Actor> actors = null;
+		List<ActorEntity> actorsEntityList = this.repository.findAll();
+		if (actorsEntityList.isEmpty()) {
+			/* return new List<Actor>(); */
+		}
+
+		for (ActorEntity actorEntity : actorsEntityList) {
+			if (actorEntity.getType() == actorType) {
 				
 			}
 		}
-    }
-    
-    public void update(Actor ac) {
-    		Optional<ActorEntity> acOpt = this.repository.findById(ac.getId());
-    		if(acOpt.isPresent())
-    		{
-    			ActorEntity acNew = acOpt.get();
-    			acNew.setName(ac.getName());
-        		acNew.setType(ac.getType());
-        		this.repository.save(acNew);
-    		}    		
-    }
-    
-    
-    public List<Actor> getAll(){
-    	//List<ActorEntity> actorsEntity = new List<ActorEntity>();
-    	//List<Actor> actors = new List<ActorEntity>();
-    	this.repository.findAll();
-    	for (int i = 0; i < array.length; i++) {
-			
-		}
-    	return ;
-    }
-	*/
+		return actors;
+	}
+
 }
