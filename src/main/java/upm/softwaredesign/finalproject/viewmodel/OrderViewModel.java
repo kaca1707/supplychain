81d8f86997254d7c282844a5cacb8c075c1f11d6
Piotr Siwinski <siwinski.piotr@outlook.com>
2019-04-08 16:27:16 +0200

    Add basic ui for creating order

---
+++ src/main/java/upm/softwaredesign/finalproject/viewmodel/OrderViewModel.java
@@ -0,0 +1,14 @@
+package upm.softwaredesign.finalproject.viewmodel;
+
+public class OrderViewModel {
+
+    private String actor;
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+}
