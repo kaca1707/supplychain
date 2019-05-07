9e3e06dd96fdf063c081118535c6c5aa3d58ce0d
Martin Zumarraga <martinzumur@gmail.com>
2019-05-07 15:10:07 +0200

    Updated Product variables

--- src/main/java/upm/softwaredesign/finalproject/model/Product.java
+++ src/main/java/upm/softwaredesign/finalproject/model/Product.java
@@ -1,4 +1,20 @@
 package upm.softwaredesign.finalproject.model;
 
 public class Product {
+
+  private String name;
+  private int count;
+
+  public Product(String name, int count){
+    this.name = name;
+    this.count = count;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getCount(){
+    return this.count;
+  }
 }
